6fb4b8d7216e5471edcadccd43e7e98f6440ebbd
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-08-26 00:31:38 +0900

    Setting validation for user model and coding test code for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'full-width characters' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'full-width characters' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'full-width katakana characters' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'full-width katakana characters' }
+    validates :phone_number, length: { maximum:11 }
+  end
+  
+  validates :admin, inclusion: { in: [false] }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'need to include @' }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'include both letters and numbers'
 end
