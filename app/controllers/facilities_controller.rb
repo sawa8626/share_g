0537c7b5732d20ad877a0e60e1e116d0fe8580f5
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-08-26 15:29:08 +0900

    Setting of access right for facilities new

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -1,4 +1,6 @@
 class FacilitiesController < ApplicationController
+  before_action :move_to_root, except: [:index]
+
   def index
   end
 
@@ -16,6 +18,10 @@ class FacilitiesController < ApplicationController
   end
 
   private
+
+  def move_to_root
+    redirect_to root_path unless current_user.admin
+  end
 
   def facility_params
     params.require(:facility).permit(:prefecture_id, :city, :name, :area, :phone_number, :price, :rule).merge(user_id: current_user.id)
