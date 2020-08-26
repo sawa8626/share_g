71ee988cdc1b6c9e27625c8f71f15112a2c1a8ab
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-08-26 15:09:36 +0900

    Implementation of facility create fuction

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -5,4 +5,19 @@ class FacilitiesController < ApplicationController
   def new
     @facility = Facility.new
   end
+
+  def create
+    @facility = Facility.new(facility_params)
+    if @facility.save
+      redirect_to root_path
+    else
+      render action: :new
+    end 
+  end
+
+  private
+
+  def facility_params
+    params.require(:facility).permit(:prefecture_id, :city, :name, :area, :phone_number, :price, :rule).merge(user_id: current_user.id)
+  end
 end
