f408a78d3602904e213c03d5b9d543b0f9c48dd5
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-09-06 18:26:36 +0900

    Modiffication of move_to_session action of teams_controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -18,7 +18,7 @@ class TeamsController < ApplicationController
   private
 
   def move_to_session
-    redirect_to root_path unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
   def team_params
