e13ddb2474bf9c116d1d58eea4d829c9ff4a113d
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-09-14 01:38:28 +0900

    Modification of team destroy function

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
-  has_many :reservations
-  has_many :team_users
-  has_many :users, through: :team_users, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  has_many :team_users, dependent: :destroy
+  has_many :users, through: :team_users
   has_one_attached :image
 
   with_options presence: true do
