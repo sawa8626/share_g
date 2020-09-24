6062d9d01190f35e87e1858b48021ad44e954dbf
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-09-24 10:03:22 +0900

    Update reservation_spec.rb

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -3,8 +3,9 @@ FactoryBot.define do
     use_application { 'バスケットボール' }
     start_time      { '2020-09-01T12:00:00' }
     end_time        { '2020-09-01T15:00:00' }
-    release         { 'true' }
+    release         { 'false' }
     association :facility
     association :user
+    association :team
   end
 end
